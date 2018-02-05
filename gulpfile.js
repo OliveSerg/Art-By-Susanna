7ba84d1a4e689ccb53a0650320873685159fbda1
OliveSerg <oliveserg@gmail.com>
2018-02-05 21:31:46 +0000

    Add foundation settings

--- gulpfile.js
+++ gulpfile.js
@@ -9,27 +9,38 @@ let gulp = require('gulp'),
 function exceptionLog(error) {
     console.log(error.toString());
     this.emit('end');
-};
+}
 
-gulp.task('styles', function () {
-    return gulp.src(themePath + '/assets/css/style.scss')
-        .pipe(sass({
-            style: 'expanded',
-            includePaths: ['node_modules/foundation-sites/scss', 'node_modules/ionicons/dist/scss', 'node_modules/motion-ui', 'node_modules/ionicons/dist/scss']
-        }))
+gulp.task('styles', function() {
+    return gulp
+        .src(themePath + '/assets/scss/style.scss')
+        .pipe(
+            sass({
+                style: 'expanded',
+                includePaths: [
+                    'node_modules/foundation-sites/scss',
+                    'node_modules/ionicons/dist/scss',
+                    'node_modules/motion-ui',
+                    'node_modules/ionicons/dist/scss'
+                ]
+            })
+        )
         .pipe(bless())
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest(themePath))
+        .pipe(gulp.dest(themePath));
 });
 
-gulp.task('scripts', function () {
-    return gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/foundation-sites/dist/js/foundation.js',
+gulp.task('scripts', function() {
+    return gulp
+        .src([
+            'node_modules/jquery/dist/jquery.js',
+            'node_modules/foundation-sites/dist/js/foundation.js',
             themePath + '/assets/js/*.js'
         ])
         .pipe(concat('scripts.js'))
         .pipe(gulp.dest(themePath));
 });
 
-gulp.task('default', function () {
-    gulp.start('styles', 'scripts')
+gulp.task('default', function() {
+    gulp.start('styles', 'scripts');
 });
