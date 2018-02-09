04bc0c82ebb905afe9629e465200bb3f092c8e01
OliveSerg <oliveserg@gmail.com>
2018-02-09 23:02:04 +0000

    Add basics for footer top

--- wp-content/themes/artbysusanna/footer.php
+++ wp-content/themes/artbysusanna/footer.php
@@ -1,6 +1,39 @@
 <footer class="site-footer" role="contentinfo">
 	<div class="footer-top">
-
+		<div class="footer-contact">
+			<h3>
+				<?php echo __('Get In Touch'); ?>
+			</h3>
+			<p>
+				<?php echo __('Got a special request or need more information?'); ?>
+			</p>
+			<dl>
+				<dt>
+					<?php echo __('Phone:');?>
+				</dt>
+				<dd>
+					<?php echo __('123-456-7890'); ?>
+				</dd>
+				<dt>
+					<?php echo __('Email:'); ?>
+				</dt>
+				<dd>
+					<?php echo __('email'); ?>
+				</dd>
+			</dl>
+		</div>
+		<?php if (has_nav_menu('footer')) : ?>
+		<div class="footer-links">
+			<h3>
+				<?php echo __('Other Places'); ?>
+			</h3>
+			<?php wp_nav_menu([
+                        'theme_location' => 'footer',
+                        'menu_id'        => 'footer-links',
+                        'menu_class'     => 'vertical menu'
+                    ]); ?>
+		</div>
+		<?php endif; ?>
 	</div>
 	<div class="footer-bottom">
 		<p>&copy;
@@ -9,7 +42,8 @@
 			</script>
 			<?php echo get_bloginfo('name') . __('. All Rights Reserved.'); ?>
 		</p>
-		<p>Development by
+		<p>
+			<?php echo __('Development by'); ?>
 			<a href="mailto:oliveserg@gmail.com?subject=Web Development Enquiry" target="_top">oliveserg@gmail.com</a>
 		</p>
 	</div>
--- wp-content/themes/artbysusanna/functions.php
+++ wp-content/themes/artbysusanna/functions.php
@@ -2,36 +2,40 @@
 
 function artbysusanna_setup()
 {
-	register_nav_menus([
-		'header' => __('Header menu', 'artbysusanna'),
-		'footer' => __('Footer menu', 'artbysusanna')
-	]);
+    register_nav_menus([
+        'header' => __('Header menu', 'artbysusanna'),
+        'footer' => __('Footer menu', 'artbysusanna')
+    ]);
 
-	add_theme_support('post-thumbnails');
-	add_image_size('xlarge', 1200, 1200, true);
+    add_theme_support('post-thumbnails');
+    add_image_size('xlarge', 1200, 1200, true);
 
-	add_theme_support('post-formats', [
-		'aside',
-		'image',
-		'video',
-		'quote',
-		'link',
-		'gallery',
-		'audio',
-	]);
+    add_theme_support('post-formats', [
+        'aside',
+        'image',
+        'video',
+        'quote',
+        'link',
+        'gallery',
+        'audio',
+    ]);
 
-	add_theme_support('custom-logo', [
-		'width' => 250,
-		'height' => 250,
-		'flex-width' => true,
-		'header-text' => ['Art By Susanna']
-	]);
+    add_theme_support('custom-logo', [
+        'width'       => 250,
+        'height'      => 250,
+        'flex-width'  => true,
+        'header-text' => ['Art By Susanna']
+    ]);
+
+    if (function_exists('register_sidebar')) {
+        register_sidebar();
+    }
 }
 add_action('after_setup_theme', 'artbysusanna_setup');
 
 function artbysusanna_scripts()
 {
-	wp_enqueue_style('artbysusanna-style', get_stylesheet_uri());
-	wp_enqueue_script('artbysusanna-script', get_theme_file_uri('/scripts.js'));
+    wp_enqueue_style('artbysusanna-style', get_stylesheet_uri());
+    wp_enqueue_script('artbysusanna-script', get_theme_file_uri('/scripts.js'));
 }
 add_action('wp_enqueue_scripts', 'artbysusanna_scripts');
